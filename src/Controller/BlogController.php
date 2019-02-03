e3169e7bbc7a8fb81781d59e93de93757dfaa175
Btb eric <btberic9393@gmail.com>
2019-02-03 11:25:37 +0100

    Systeme de securité ok!

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index()
+    {
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->render('blog/admin.html.twig');
+        }
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     *
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig');
+    }
+}
